bd03412de3c487f7377ff3d90f59e667fa2815a6
Rasmus Anthony Nielsen <rale.dive@gmail.com>
2020-05-31 10:01:38 +0200

    basic bot following https://github.com/hybridgroup/gobot/tree/master/platforms/raspi.

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/gofrs/uuid v3.3.0+incompatible // indirect
 	github.com/hashicorp/go-multierror v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	gobot.io/x/gobot v1.14.0 // indirect
+	gobot.io/x/gobot v1.14.0
+	periph.io/x/periph v3.6.2+incompatible
 )
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/raspi"
+)
 
 func main() {
-	fmt.Println("Hello Roller")
+	adaptor := raspi.NewAdaptor()
+	led7 := gpio.NewLedDriver(adaptor, "7")
+
+	blinkLed := func() {
+		gobot.Every(1*time.Second, func() {
+			led7.Toggle()
+		})
+	}
+
+	robot := gobot.NewRobot("Blink Test", []gobot.Connection{adaptor}, []gobot.Device{led7}, blinkLed)
+
+	robot.Start()
 }
